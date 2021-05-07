cb5c7da10f77c3952c5f6061ec30629ec8caacaf
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-05-07 11:30:31 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Button from 'primevue/button';
+import PrimeVue from 'primevue/config';
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(PrimeVue);
+
+app.component('Button', Button);
+
+app.mount("#app")
