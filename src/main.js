217bd5dd3b5b259ac69b9ace299a3674b6723598
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-05-07 12:25:04 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import { createApp } from 'vue';
 import PrimeVue from 'primevue/config';
 import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
-// import Toast from 'primevue/toast';
-// import ToastService from 'primevue/toastservice';
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice';
 
 import "primevue/resources/themes/saga-blue/theme.css";
 import "primevue/resources/primevue.min.css";
@@ -15,8 +15,10 @@ import App from './App.vue';
 const app = createApp(App);
 
 app.use(PrimeVue);
+app.use(ToastService);
 
 app.component('Button', Button);
 app.component('InputText', InputText);
+app.component('Toast', Toast);
 
 app.mount("#app")
